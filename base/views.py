cd6822e8558d49649c740234396e9a58feb12961
jsegovia <jsegovia@boxcustodia.com>
2023-06-20 18:35:29 -0300

    Agrego vista Details

--- base/views.py
+++ base/views.py
@@ -1,9 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
 from .models import Task
 
 
 # Create your views here.
 class TaskList(ListView):
     model = Task
+    context_object_name = "tasks"
+
+
+# Vista que hereda de la vista Details
+class TaskDetail(DetailView):
+    model = Task
+    context_object_name = "task"
+    template_name = "base/task.html"  # Establezco nombre a la planilla
